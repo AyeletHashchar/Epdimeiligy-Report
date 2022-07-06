a342b350f8eea41931221d5a497e8e21bd6cfba5
Esther Weller <sstt1414@gmail.com>
2022-07-06 15:47:58 +0300

    add api solution

---
+++ EpidemioligyApi/Week_2/Controllers/PatientController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http.Cors;
+using Week_2.Models;
+
+namespace Week_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        [HttpGet]
+        public  List<Report> GetAllReports()
+        {
+            List<Report> allReports = new List<Report>();
+            Data.Data.patients.ForEach((p => allReports.AddRange(p.reports)));
+            return allReports;
+        }
+
+        [HttpGet("{id}")]
+        public List<Report> GetReportsById(int id)
+        {
+            Patient curPatien = Data.Data.patients.FirstOrDefault(p => p.id == id);
+            if(curPatien != null)
+                return curPatien.reports;
+            return new List<Report>();
+        }
+
+        [HttpGet]
+        [Route("getReportsByCity/{city}")]
+
+        public List<Report> GetReportsByCity(string city)
+        {
+            if (city == "-")
+                city = "";
+            List<Report> reports = new List<Report>();
+            foreach (Patient patient in Data.Data.patients)
+            {
+                foreach (Report report in patient.reports)
+                {
+                    if (report.city.Contains(city))
+                        reports.Add(report);
+                }
+            }
+            return reports;
+        }
+
+        [HttpPost]
+        [Route("{patientId}")]
+        public void PostReport([FromBody] Report report, int patientId)
+        {
+            Patient curPatient = Data.Data.patients.FirstOrDefault(p => p.id == patientId);
+            if (curPatient != null)
+                curPatient.reports.Add(report);
+            else
+            {
+                Patient newPatient = new Patient(new List<Report> { report }, patientId);
+                Data.Data.patients.Add(newPatient);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{patientId, ReportId}")]
+        public void Delete(int patientId, int ReportId)
+        {
+            foreach (Patient patient in Data.Data.patients)
+            {
+                if (patient.id == patientId)
+                {
+                    foreach (Report report in patient.reports)
+                    {
+                        if (report.id == ReportId)
+                            patient.reports.Remove(report);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ EpidemioligyApi/Week_2/Data/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Week_2.Models;
+
+namespace Week_2.Data
+{
+    public class Data
+    {
+        public static Report r1 = new Report() { city = "jerusalem", location = "library", endDate = new DateTime(1974, 7, 10, 9, 10, 24), startDate = new DateTime(1974, 7, 10, 7, 10, 24) };
+        public static Report r2 = new Report() { city = "jerusalem", location = "restraunt", endDate = new DateTime(2002, 11, 24, 10, 31, 2), startDate = new DateTime(2002, 11, 24, 11, 45, 32) };
+        public static Report r3 = new Report() { city = "tel aviv", location = "park", endDate = new DateTime(2022, 3, 17, 16, 2, 15), startDate = new DateTime(2022, 3, 17, 20, 16, 54) };
+        public static Report r4 = new Report() { city = "bney brak", location = "garden", endDate = new DateTime(2017, 8, 19, 12, 10, 25), startDate = new DateTime(2017, 8, 19, 14, 1, 2) };
+        public static Report r5 = new Report() { city = "bney brak", location = "school", endDate = new DateTime(2016, 1, 3, 8, 3, 44), startDate = new DateTime(2016, 1, 3, 13, 7, 29) };
+
+        public static List<Patient> patients = new List<Patient>()
+        {
+            { new Patient(new List<Report> { r1, r2, r3 }, 1)},
+            { new Patient(new List<Report> { r1, r5 }, 2)},
+            { new Patient(new List<Report> { r2, r4, r5 }, 3)},
+            { new Patient(new List<Report> { r1, r2, r3, r4 }, 4)},
+
+        };
+    }
+}
