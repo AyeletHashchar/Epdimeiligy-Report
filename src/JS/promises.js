70b31c722b0f3714a837b267ed7284f81929e893
Esther Weller <sstt1414@gmail.com>
2022-07-06 12:18:09 +0300

    commit stringsFeature branch

--- src/JS/promises.js
+++ src/JS/promises.js
@@ -4,12 +4,26 @@ function makeAllCups(words){
         words?.forEach(word => {
             newArr.push(word.toUpperCase());
         });
-        if(newArr.length > 0)
-            resolve(newArr);
-        reject("ERR: Translation failed. Possibly reason: no input")
+        if(newArr.length > 0){
+            sortWords(newArr)
+            .then((res)=>{resolve(res)})
+            .catch((err)=>{reject(err)});
+        }
+        else reject("ERR: Translation failed. Possibly reason: no input")
+    })
+}
+
+function sortWords(words){
+    let newArr = words;
+    return new Promise((resolve, reject)=>{
+        if(newArr.includes(1)){
+            reject("ERR: Unvalid Input. " + word + " is not a string.")
+        }
+        newArr.sort();
+        resolve(newArr);
     })
 }
 
 makeAllCups(["aaa", "ccc", "bBb"])
-.then((res)=>{alert(res)})
+.then((resolve)=>{alert(resolve)})
 .catch((err)=>{console.error(err)});
